e6cef4dac2af654b44bfca38bd2b6f6465e1b4fc
Sergey Ivanov <mailto.sergeyivanov@gmail.com>
2023-06-15 16:40:10 +0300

    Изменил кодировку файлов на ANSI и добавил код в  функцию  ShowQestion

--- CommandProjectP21/Source.cpp
+++ CommandProjectP21/Source.cpp
@@ -1,13 +1,53 @@
 #include<iostream>
 #include<Windows.h>
 #include<io.h>
+#include<direct.h>
 using namespace std;
 
 char theme[20];
 
 void ShowQestion()
 {
-	cout << theme;
+	FILE* fp;
+	char str[100];
+	char answerFromFile;
+	char answerFromUser;
+	int cnt = 0;
+	int var1 = _chdir("Questions");
+
+	fopen_s(&fp, theme, "r");
+
+	if (fp != NULL)
+	{
+		do
+		{
+			system("cls");
+
+			for (size_t i = 0; i < 6; i++)
+			{
+				fgets(str, 100, fp);
+
+				if (i==5)
+				{
+					answerFromFile = str[0];
+					
+				}
+				else
+				{
+					cout << str;
+				}
+			}
+
+			cnt++;
+
+			cout << "Введите ответ - ";
+
+			cin >> answerFromUser;
+
+		} while (cnt<5);
+
+		fclose(fp);
+	}
 }
 
 void Select_Theme()
@@ -31,17 +71,20 @@ void Select_Theme()
 	cout << "Выберите тему" << endl;
 
 	cin >> theme;
-
+	
 	ShowQestion();
 
 }
 
 void Viktorina()
 {
+	//SetConsoleCP(1251);
+	//SetConsoleOutputCP(1251);
+
 	setlocale(LC_ALL, "ru");
 
 	cout << "Viktorina" << endl;
-	
+
 	Select_Theme();
 }
 
